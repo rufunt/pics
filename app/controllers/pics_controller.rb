9ea3e75cdacdd1484cfd50f92a469373a0a23bda
rufunt <jazzoo@mail.ru>
2018-07-05 16:24:53 +0300

    Add app view and actions for controller

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,12 +4,22 @@ class PicsController < ApplicationController
     
   end
 
+  def show
+    @pic = Pic.find(params[:id])
+  end
+
   def new
     @pic = Pic.new
   end
 
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: "Yes, it was posted!"
+    else
+      render 'new'
+    end
   end
 
   private
