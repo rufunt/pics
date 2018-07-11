2cbe1aff0968d5ba567c4a8833003bd45b603cc2
rufunt <jazzoo@mail.ru>
2018-07-11 16:11:15 +0300

    Bootstrap style

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -2,7 +2,8 @@ class Pic < ApplicationRecord
   #acts_as_votable
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>" }
   
+  has_attached_file :image, styles: { medium: "300x300>" }, :default_url => ":style/rails1.jpg", validate_media_type: false
+  #has_attached_file :image, styles: { :medium => "300x300>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
